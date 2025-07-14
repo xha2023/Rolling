4e03b85435b566b7e2094cdd80941323810705ec
apple <apple@appleui-MacBookAir.local>
2025-07-14 17:15:52 +0900

    Feat: Add Modal

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ const config = {
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest"
   ],
