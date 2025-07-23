99db80dc6d5455bc77e19c09eb7a9fb47d49539d
Heesu <nizoo0516@gmail.com>
2025-07-23 12:33:56 +0900

    Fix: MessageCard.jsx 파일과 관련한 에러 해결

--- src/components/card/MessageCard.jsx
+++ src/components/card/MessageCard.jsx
@@ -1,9 +1,10 @@
 import React, { forwardRef } from 'react';
+import styled from 'styled-components';
 import Button from '../button/Button.jsx';
 import Badge from '../badge/TextBadge';
 import defaultProfile from '../../assets/svg/default_profile.svg';
 import { formatDate } from '../../utils/FormateDate.js';
-import { Helmet } from 'react-helmet-async';
+
 import {
   CardContainer,
   Header,
@@ -11,60 +12,10 @@ import {
   HeaderInfo,
   FromText,
   NameText,
-  StatusBadge,
   MessageContent,
   MessageText,
   DateText,
 } from './MessageCard.styled.js';
-
-import binIcon from '../../assets/svg/bin.svg';
-
-
-const MessageCard = ({
-  messageId,
-  profileImage,
-  name = '김동훈',
-  status = '동료',
-  message = '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!',
-  date = '2025.07.12',
-  font,
-  isEditing,
-  onDelete,
-}) => {
-  const formattedDate = formatDate(date);
-  return (
-    <CardContainer>
-      <Header>
-        <ProfileImage>
-          {profileImage ? (
-            <img src={profileImage} alt="Profile" />
-          ) : (
-            <img src={defaultProfile} alt="Profile" />
-          )}
-        </ProfileImage>
-        <HeaderInfo>
-          <FromText>
-            From. <NameText>{name}</NameText>
-          </FromText>
-          <StatusBadge $status={status}>{status}</StatusBadge>
-        </HeaderInfo>
-        {isEditing && (
-          <DeleteCardButton
-            onClick={() => onDelete(messageId)}
-            variant="outlined"
-            size="icon"
-          >
-            <img src={binIcon} alt="공유" />
-          </DeleteCardButton>
-        )}
-      </Header>
-
-      <MessageContent>
-        <MessageText
-          $font={font}
-          dangerouslySetInnerHTML={{ __html: message }}
-        />
-      </MessageContent>
 
 const MessageCard = forwardRef(
   (
@@ -104,22 +55,23 @@ const MessageCard = forwardRef(
             <Badge label={status} />
           </HeaderInfo>
           {isEditing && (
-            <Button
+            <DeleteCardButton
               onClick={(e) => {
-                e.stopPropagation(); // 카드 클릭 이벤트 방지
+                e.stopPropagation();
                 onDelete(messageId);
               }}
               variant="icon"
             >
-              🗑️
-            </Button>
+              <img src={binIcon} alt="공유" />
+            </DeleteCardButton>
           )}
         </Header>
 
         <MessageContent>
-          <MessageText 
+          <MessageText
             $font={font}
-             dangerouslySetInnerHTML={{ __html: message }} />
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
         </MessageContent>
 
         <DateText>{formattedDate}</DateText>
